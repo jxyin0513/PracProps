a290c4200bbdbc9e4e2381151586bdd7e823cd17
jxyin0513 <101522265+jxyin0513@users.noreply.github.com>
2022-06-14 13:35:25 -0400

    BaseStats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Showcase from './Showcase';
-import './App.css';
+import Showcase from "./Showcase";
+import Basestats from "./BaseStats";
+import "./App.css";
 
 function App() {
+  const baseStats = {
+    hp: 45,
+    attack: 49,
+    defense: 49,
+    spAttack: 65,
+    spDef: 65,
+    speed: 45
+  };
+  function handleClick() {
+    alert(
+      `Special Stats\n\tSpecial Attack: ${baseStats.spAttack}\n\tSpecial Defense: ${baseStats.spDef}`
+    );
+  }
   return (
-    <div className='main-wrapper background'>
+    <div className="main-wrapper background">
       <Showcase />
+      <Basestats stats={baseStats} clicker={handleClick} />
     </div>
   );
 }
